9f2e48c5c40c2c3e4b3fb5566c91f6bfa58ec42d
Arkitu <85173315+Arkitu@users.noreply.github.com>
2023-09-13 19:44:32 +0200

    fix new gradio

--- src/getEmojiFromName.ts
+++ src/getEmojiFromName.ts
@@ -1,6 +1,6 @@
 import { client as gradioClient } from "@gradio/client";
 
 export default async function getEmojiFromName(name: string) {
-    const app = await gradioClient("https://easrng-text-to-emoji.hf.space/");
+    const app = await gradioClient("https://easrng-text-to-emoji.hf.space/", {});
     return ((await app.predict("/predict", [name])) as any).data[0] as string;
 }
